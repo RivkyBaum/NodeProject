2870c9e926b07004cffd681620765e466f5625f6
RivkyBaum <rivky3254@gmail.com>
2023-05-16 12:53:31 +0300

    Create UserController.js

---
+++ controller/UserController.js
@@ -0,0 +1,18 @@
+const User=require('../User')
+var listUsres=[
+    {id:1,name:"aaa",email:"fff@ddd"},
+    {id:2,name:"aaa",email:"fff@ddd"},
+    {id:3,name:"aaa",email:"fff@ddd"},
+
+]
+module.exports={
+    getById:(req,res)=>{
+        let _id=req.params.id
+        res.status(200).send(listUsres.find(a=>a.id==_id))
+    },
+    Add:(req,res)=>{
+        let newUser=req.body
+        listUsres.push(newUser)
+        res.status(200).send(newUser)
+    }
+}
