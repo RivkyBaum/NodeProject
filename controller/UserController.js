635fe33b5dfc8708180718d85913b37ce1b3d607
RivkyBaum <rivky3254@gmail.com>
2023-05-18 10:49:18 +0300

    hhh

--- controller/UserController.js
+++ controller/UserController.js
@@ -1,44 +1,58 @@
 const User = require('../User')
+const axios = require('axios');
+
+//data -מערך משתמשים 
 var listUsres = [
     { id: 1, name: "lea", email: "fff@ddd", phone: "999999" },
     { id: 2, name: "Yehudit", email: "fff@ddd", phone: "999999" },
-    { id: 3, name: "pppppppppppppppp", email: "fff@ddd", phone: "055332545" },
-
+    { id: 3, name: "Rivky", email: "fff@ddd", phone: "055332545" },
+//oooo
 ]
-const axios = require('axios');
-function isEmailValid(email)
-{
-    // Check if email is a string
-    if (typeof email !== 'string') {
-        return false;
-    }
-    // Check if email has a valid format
-    if (!/^[a-zA-Z0-9]+[a-zA-Z0-9._-]*@[a-zA-Z0-9]+\.[a-zA-Z]{2,}$/.test(email)) {
-        return false;
-    }
-    return true;
-}
+
 module.exports = {
-    getById: (req, res) => {
+    getById: async (req, res) => {
         let _id = req.params.id
         res.status(200).send(listUsres.find(a => a.id == _id))
         res.send(listUsres[req.params._id])
     },
-    getAll: (req, res) => {
+    getAll: async (req, res) => {
         res.send(listUsres)
     },
-   
-    Delete:(req,res)=>{
+    //חייבים לעשות את הפונקציה אסינכרונית כי זה עושה כמה קריאות שרת
+    Add: async (req, res) => {
+        try {
+          const UrlUuid = "https://www.uuidgenerator.net/api/guid.json"
+          let new_user = req.body;
+          const response = await axios.get(`https://phonevalidation.abstractapi.com/v1/?api_key=8cf0533e9612479b89fa5b6065107408&phone=${req.body.phone}`);
+          console.log(response.data.valid)
+          if (response.data.valid == false) {
+            res.send("not validate")
+          } else {
+            // אם המס' תקין מוסיפים uuid
+            const responseFromUuid = await axios.get(UrlUuid);
+            new_user.id = responseFromUuid.data;
+            listUsres.push(new_user);
+            res.send(new_user);
+          }
+        } catch (error) {
+          console.log(error);
+        }
+      },
 
-        const userId = req.params.id;
-        const index = listUsres.find (user => user.id === userId);
-        if (index !== -1) {
+    //לא עובד!!
+    Delete: async (req, res) => {
+        try {
+          const userId = req.params.id;
+          const index = listUsres.findIndex(user => user.id === userId);
+          if (index !== -1) {
             listUsres.splice(index, 1);
             res.status(200).send(`User with ID ${userId} deleted`);
           } else {
             res.status(404).send(`User with ID ${userId} not found`);
           }
+        } catch (error) {
+          console.log(error);
+        }
     }
    
-
 }
