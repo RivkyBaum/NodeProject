2653714e3f33d285bdce7f015109dd85ee03d2e9
RivkyBaum <rivky3254@gmail.com>
2023-05-18 10:37:19 +0300

    lll

--- controller/UserController.js
+++ controller/UserController.js
@@ -27,64 +27,7 @@ module.exports = {
     getAll: (req, res) => {
         res.send(listUsres)
     },
-    Add: (req, res) => {
-      
-        let _name=req.body.name
-        let _email=req.body.email
-        let _phone=req.body.phone
-        if(isEmailValid(req.body.email))
-        console.log("valid")
-      
-        let newUser = {_name,_email,_phone}
-        let phoneUser = req.body.phone
-        
-        // axios.get(`https://phonevalidation.abstractapi.com/v1/?api_key=66a5643d396849e489687cbe7aafb0e6&phone=${phoneUser}`)
-        //     .then(response => {
-        //         console.log(response.data);
-                listUsres.push(newUser)
-                // res.status(200).send(newUser)
-                res.send(listUsres)
-
-            // })
-            // .catch(error => {
-            //     console.log(error);
-            //     res.status(404).send(error)
-            // });
-    
-
-    },
-    Add1:(req,res)=>{
-        const { name,email,phone } = req.body
-        //כל אובייקט json
-        //שהמפתח והערך הם בעלי אותו שם
-        //מספיק לכתוב פעם אחת בלבד
-        User.find({ email })
-            .then((listUsres) => {
-                console.log(users);
-                if (listUsres.length >= 1) {
-                    return res.status(409).send({ message: 'Email is already exists' })
-                }
-
-               
-                    const user = new User({
-                        id:hash,
-                        name,
-                        email,
-                        phone 
-                    })
-                    user.save()
-                        .then(() => {
-                            res.status(200).send('user created!')
-                        })
-                        .catch((error) => {
-                            res.status(500).send({ error: error.message })
-                        })
-                })
-            
-            .catch((error) => {
-                res.status(500).send({ error: error.message })
-            })
-    },
+   
     Delete:(req,res)=>{
 
         const userId = req.params.id;
