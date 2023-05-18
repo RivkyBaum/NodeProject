fbe46d9bddf166241da27dd185cab641fb1dac96
chayaRivkaErenfeld <erenfeld226@gmail.com>
2023-05-18 10:25:22 +0300

    Adding asynchronous functions3

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const morgan = require('morgan')
 const bodyParser = require('body-parser')
 //יצירת שרת
 const app = express()
-const port=3000
+const port=3006
 //midlleware:
 //הרשאות
 app.use((req, res, next) => {
